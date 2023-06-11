f5d69ef13d1446729e71324ee2d99f6549fab42d
Puza24 <okmanovic.ognjen@gmail.comgit config --global user.email dokmanovic.ognjen@gmail.com>
2023-06-11 14:13:12 +0200

    feature/CL-003: Lection 3

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -11,5 +11,15 @@ describe('My first test suite', function () {
         cy.get('.product:visible').should('have.length', 4)
         cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
         //selektor parent, nadji unutar parenta sve product child slelektore, izvuci zeljeni selektor preko indexa, contains - nadji selektor sa tim textom i klikni da to dugme sa tim textom
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+            //za svaki element odradi loop, ako ima 4 producta, loop ce se odraditi 4 puta, svkai loop produkt pada u $el
+            const vegText = $el.find('h4.product-name').text();
+            // uhvatili smo selektor producta sa textom od product-a
+            if (vegText.includes('Cashews')) {
+                //ako je text od producta jednak Cashews... 
+                cy.wrap($el).contains('ADD TO CART').click()
+                //onda klikni ADD to Cart button i cy.wrap smo ubacili jer od novije verzije cypressa mora tako inace ne bi radila metoda za click
+            }
+        })
     })
 })
