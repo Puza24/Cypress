9516fc4dc702f05edd6e99a75fe00b43c64ee71c
Puza24 <okmanovic.ognjen@gmail.comgit config --global user.email dokmanovic.ognjen@gmail.com>
2023-06-11 13:56:56 +0200

    feature/CL-001: Test1 update

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -9,5 +9,6 @@ describe('My first test suite', function () {
         cy.get('.search-keyword').type('ca')
         cy.wait(2000)
         cy.get('.product:visible').should('have.length', 4)
+        cy.get('.products').find('.product').should('have.length', 4)
     })
 })
