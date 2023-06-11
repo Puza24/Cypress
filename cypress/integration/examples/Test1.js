02bdf2da8b96e8fa07adc1514293762ca10fb01d
Puza24 <okmanovic.ognjen@gmail.comgit config --global user.email dokmanovic.ognjen@gmail.com>
2023-06-11 14:04:30 +0200

    feature/CL-002: Lection 2

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -9,5 +9,7 @@ describe('My first test suite', function () {
         cy.get('.search-keyword').type('ca')
         cy.wait(2000)
         cy.get('.product:visible').should('have.length', 4)
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+        //selektor parent, nadji unutar parenta sve product child slelektore, izvuci zeljeni selektor preko indexa, contains - nadji selektor sa tim textom i klikni da to dugme sa tim textom
     })
 })
